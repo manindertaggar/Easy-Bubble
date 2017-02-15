0d836f34407f8fc1181b6ffb4a00bcf3406e7ad4
pkdhankher <pkdhankher@gmail.com>
2017-02-15 19:30:02 +0530

    installed apps list added to main activity

---
+++ app/src/main/java/com/eworl/easybubble/Adapter/AdapterClass.java
@@ -0,0 +1,62 @@
+package com.eworl.easybubble.Adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.eworl.easybubble.R;
+import com.eworl.easybubble.activities.MainActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dhankher on 2/15/2017.
+ */
+public class AdapterClass extends RecyclerView.Adapter<AdapterClass.viewHolder> {
+
+    Context context;
+    ArrayList<Bitmap> appIcons;
+    ArrayList<String> appNames;
+
+    public AdapterClass(Context context, ArrayList<Bitmap> appIcons, ArrayList<String> appNames) {
+        this.context = context;
+        this.appIcons = appIcons;
+        this.appNames = appNames;
+
+    }
+
+    @Override
+    public viewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View myView = LayoutInflater.from(context).inflate(R.layout.app_list_layout, parent, false);
+        return new viewHolder(myView);
+    }
+
+    @Override
+    public void onBindViewHolder(viewHolder holder, int position) {
+        holder.appNameTV.setText(appNames.get(position));
+        holder.appIconIV.setImageBitmap(appIcons.get(position));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return appNames.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        TextView appNameTV;
+        ImageView appIconIV;
+
+        public viewHolder(View itemView) {
+            super(itemView);
+            appNameTV = (TextView) itemView.findViewById(R.id.appNameTV);
+            appIconIV = (ImageView)itemView.findViewById(R.id.appIconIV);
+        }
+
+    }
+}
--- app/src/main/java/com/eworl/easybubble/activities/MainActivity.java
+++ app/src/main/java/com/eworl/easybubble/activities/MainActivity.java
@@ -1,18 +1,22 @@
 package com.eworl.easybubble.activities;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.eworl.easybubble.Adapter.AdapterClass;
 import com.eworl.easybubble.LayoutParamGenerator;
 import com.eworl.easybubble.PermissionManager;
 import com.eworl.easybubble.R;
@@ -21,43 +25,42 @@ import com.eworl.easybubble.bubbles.MasterBubble;
 import com.eworl.easybubble.bubbles.SubBubble;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
     private ArrayList<Integer> imageIcons = new ArrayList<>();
+    private ArrayList<String> appsList = new ArrayList<>();
     private MasterBubble masterBubble;
     private Button startServiceButton;
     private ViewManager viewManager;
     private TextView appsListTV;
+    private ImageView imageIcon;
+    private RecyclerView recyclerView;
+    private AdapterClass adapterClass;
+    ArrayList<String> appNames;
+    ArrayList<Bitmap> appIcons;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
         startServiceButton = (Button) findViewById(R.id.button);
-        appsListTV = (TextView) findViewById(R.id.appsList);
-
-        int flags = PackageManager.GET_META_DATA |
-                PackageManager.GET_SHARED_LIBRARY_FILES |
-                PackageManager.GET_UNINSTALLED_PACKAGES;
-
-        PackageManager pm = getPackageManager();
-        List<ApplicationInfo> applications = pm.getInstalledApplications(flags);
-//        List<ApplicationInfo> installedApps = new ArrayList<ApplicationInfo>();
-        for (ApplicationInfo appInfo : applications) {
-            if ((appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
-
-                Log.d(TAG, "installedApps: "+applications);
-                // System application
-            } else {
-                // Installed by user
-            }
-        }
 
 
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        appNames = new ArrayList<>();
+        appIcons = new ArrayList<>();
+        getInstalledApplication(this);
 
+
+        adapterClass = new AdapterClass(this,appIcons,appNames);
+        recyclerView.setAdapter(adapterClass);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
 
@@ -82,13 +85,42 @@ public class MainActivity extends Activity {
 
         }
 
-
     }
+
     public void startServive(View view) {
         viewManager.addView(masterBubble.getView(), LayoutParamGenerator.getNewLayoutParams());
-        finish();
+//        finish();
     }
 
+
+    public List<ApplicationInfo> getInstalledApplication(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        List<ApplicationInfo> apps = packageManager.getInstalledApplications(0);
+        List<ApplicationInfo> appInfoList = new ArrayList();
+        for (ApplicationInfo info : apps) {
+            if (packageManager.getLaunchIntentForPackage(info.packageName) != null) {
+                appInfoList.add(info);
+
+            }
+        }
+        Collections.sort(appInfoList, new ApplicationInfo.DisplayNameComparator(packageManager));
+        Log.d(TAG, "getInstalledApplication: " + appInfoList);
+        int count = appInfoList.size();
+        Log.d(TAG, "count: "+count);
+        for (int i = 0; i < count; i++) {
+            String label = (String) packageManager.getApplicationLabel(appInfoList.get(i));
+            appNames.add(label);
+            Drawable icon = packageManager.getApplicationIcon(appInfoList.get(i));
+            Bitmap myLogo = ((BitmapDrawable) icon).getBitmap();
+            appIcons.add(myLogo);
+//            imageIcon.setImageBitmap(myLogo);
+            Log.d(TAG, "label: " + myLogo);
+        }
+        return appInfoList;
+
+    }
 }
+
+
 
 
