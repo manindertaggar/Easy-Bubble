a83f7f0eb5d8164d5cc8f929f91a41efd12bdb96
pkdhankher <pkdhankher@gmail.com>
2017-02-21 10:48:34 +0530

    action added to list

--- app/src/main/java/com/eworl/easybubble/Adapter/AdapterClass.java
+++ app/src/main/java/com/eworl/easybubble/Adapter/AdapterClass.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.eworl.easybubble.R;
 import com.eworl.easybubble.activities.MainActivity;
 
